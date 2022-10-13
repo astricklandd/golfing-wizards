2f6b46d7153dc203c2b4b95cf7c81de345affc42
astricklandd <111793985+astricklandd@users.noreply.github.com>
2022-10-13 15:39:33 -0700

    new code/ needs edited

---
+++ scripts/data.js
@@ -0,0 +1,17 @@
+const golfGames = [
+  {
+    game: "something",
+    author: "someone",
+    url: "somewhere",
+  },
+  {
+    game: "something2",
+    author: "someone2",
+    url: "somewhere2",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+];
